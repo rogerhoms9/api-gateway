981abf19371777dc3144e4588bf3f807eeed4be5
RogerHoms <rhoms@novatilu.com>
2021-03-15 19:59:05 +0100

    Redirigir totes les crides de l'api correctament

--- src/main/java/gateway/AplicationApiApplication.java
+++ src/main/java/gateway/AplicationApiApplication.java
@@ -24,10 +24,14 @@ public class AplicationApiApplication {
 	public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
 		String httpUri = uriConfiguration.getHttpbin();
 		return builder.routes()
-			.route(p -> p.path("/get").filters(f -> f.addRequestHeader("Hello", "World")).uri(httpUri))
-			.route(p -> p.host("*.hystrix.com").filters(f -> f.hystrix(config -> config.setName("mycmd").setFallbackUri("forward:/fallback"))).uri(httpUri))
-			.route(p -> p.path("/ss").filters(f -> f.addRequestHeader("Hello", "World")).uri(httpUri))
+			//.route(p -> p.path("/get").filters(f -> f.addRequestHeader("Hello", "World")).uri(httpUri)) Exemple
 			.route(r -> r.path("/museums").uri("https://musea-api.herokuapp.com/")
+	            )	
+			.route("museu_id",
+	                    route -> route
+	                            .path("/museums/**")
+	                            .filters(f -> f.rewritePath("/museums/(?<RID>.*)", "/museums/${RID}"))
+	                            .uri("https://musea-api.herokuapp.com/")
 	            )
 			.build();
 	}
